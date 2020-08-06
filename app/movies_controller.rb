0bbff720299a2453f098db0cf72b49483a169c53
Ronald Burrell Jr <ronburrell05@gmail.com>
2020-08-06 13:55:20 +0000

    Automatically backed up by Learn

--- app/movies_controller.rb
+++ app/movies_controller.rb
@@ -61,7 +61,7 @@ end
 def can_find_using_where_clause_and_be_sorted
   # For this test return all movies released after 2002 and ordered by 
   # release date descending
-  movies = Movies.all
+  movies = Movie.all
   movies.where("release_date: > 2002")
 end
 
